6def02a8c95ac81bccd3de3beaf04709a5c42e3a
lwj <lwj102153@gmail.com>
2019-09-22 00:44:57 +0800

    add get random_number class

---
+++ include/random_generator.h
@@ -0,0 +1,27 @@
+//
+// Created by lwj on 2019/9/22.
+//
+
+#ifndef UDPTUN_RANDOM_GENERATOR_H
+#define UDPTUN_RANDOM_GENERATOR_H
+
+#include <boost/noncopyable.hpp>
+#include <cstdint>
+
+class RandomNumberGenerator : public boost::noncopyable {
+ public:
+  static RandomNumberGenerator *GetInstance();
+  int32_t GetRandomNumber(uint32_t& random_number);
+  int32_t GetRandomNumberNonZero(uint32_t& random_number);
+
+ protected:
+  struct Obj_Creator {
+    Obj_Creator();
+  };
+  static Obj_Creator obj_creator_;
+  RandomNumberGenerator();
+  ~RandomNumberGenerator();
+  int32_t random_number_fd_;
+};
+
+#endif //UDPTUN_RANDOM_GENERATOR_H
